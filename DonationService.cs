459efc88863b3b92d55309c721ba5fed4a777d32
Bokamoso <ST10379296@RCCONNECT.EDU.ZA>
2025-10-31 23:34:15 +0200

    Unit Testing Implementation Integration Testing Implementation

---
+++ DonationService.cs
@@ -0,0 +1,23 @@
+﻿using DisasterManagementApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DisasterManagementApp.Services
+{
+    public class DonationService
+    {
+        public List<Donation> Donations { get; set; } = new List<Donation>();
+
+        public void AddDonation(Donation donation)
+        {
+            Donations.Add(donation);
+        }
+
+        public decimal CalculateTotalMonetaryDonations()
+        {
+            return Donations
+                .Where(d => d.DonationType == "Monetary" && d.Amount.HasValue)
+                .Sum(d => d.Amount.Value);
+        }
+    }
+}
---
+++ VolunteerService.cs
@@ -0,0 +1,38 @@
+﻿using DisasterManagementApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DisasterManagementApp.Services
+{
+    public class VolunteerService
+    {
+        public List<Volunteer> Volunteers { get; set; } = new List<Volunteer>();
+        public List<VolunteerAssignment> Assignments { get; set; } = new List<VolunteerAssignment>();
+
+        public void AddVolunteer(Volunteer volunteer)
+        {
+            Volunteers.Add(volunteer);
+        }
+
+        public Volunteer GetVolunteerById(int id)
+        {
+            return Volunteers.FirstOrDefault(v => v.VolunteerID == id);
+        }
+
+        public void AssignVolunteer(int volunteerId, int projectId)
+        {
+            Assignments.Add(new VolunteerAssignment
+            {
+                AssignmentID = Assignments.Count + 1,
+                VolunteerID = volunteerId,
+                ProjectID = projectId,
+                AssignedDate = System.DateTime.Now
+            });
+        }
+
+        public List<VolunteerAssignment> GetAssignmentsForVolunteer(int volunteerId)
+        {
+            return Assignments.Where(a => a.VolunteerID == volunteerId).ToList();
+        }
+    }
+}
